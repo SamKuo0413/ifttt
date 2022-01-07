1f02a0ea1ef3fd8ad4fc3dfa7600bd587cda7f1f
SamKuo0413 <89826538+SamKuo0413@users.noreply.github.com>
2022-01-07 15:02:23 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
+Obloq_http.Obloq_WIFI_setup(
+SerialPin.P8,
+SerialPin.P12,
+"yourSSID",
+"yourPASSWORD"
+)
+let 測試 = 0
 basic.forever(function () {
-	
+    basic.pause(10000)
+    測試 += 1
+    Obloq_http.sendToIFTTT(
+    "homework",
+    "dA92WK-746PJvhTzJIVecC",
+    convertToText(測試)
+    )
 })
